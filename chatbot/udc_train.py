cda28bbd6c33732521fcba7579398c92ce3a6529
pangjiuzala <1325530876@qq.com>
2017-05-06 17:35:02 +0800

    remove chatbot/prepare_data.py

--- chatbot/udc_train.py
+++ chatbot/udc_train.py
@@ -1,48 +1,55 @@
-import sys
-import numpy as np
+import os
+import time
 import tensorflow as tf
 import udc_model
 import udc_hparams
+import udc_metrics
+import udc_inputs
 from com.tensorflowTest.chatbot.models.dual_encoder import dual_encoder_model
 
-tf.flags.DEFINE_string("model_dir", None, "Directory to load model checkpoints from")
-tf.flags.DEFINE_string("vocab_processor_file", "./data/vocab_processor.bin", "Saved vocabulary processor file")
+tf.flags.DEFINE_string("input_dir", "./data", "Directory containing input data files 'train.tfrecords' and 'validation.tfrecords'")
+tf.flags.DEFINE_string("model_dir", None, "Directory to store model checkpoints (defaults to ./runs)")
+tf.flags.DEFINE_integer("loglevel", 20, "Tensorflow log level")
+tf.flags.DEFINE_integer("num_epochs", None, "Number of training Epochs. Defaults to indefinite.")
+tf.flags.DEFINE_integer("eval_every", 2000, "Evaluate after this many train steps")
 FLAGS = tf.flags.FLAGS
 
-if not FLAGS.model_dir:
-    print("You must specify a model directory")
-    sys.exit(1)
+TIMESTAMP = int(time.time())
 
-def tokenizer_fn(iterator):
-    return (x.split(" ") for x in iterator)
-# Load vocabulary
-vp = tf.contrib.learn.preprocessing.VocabularyProcessor.restore(
-                                                                FLAGS.vocab_processor_file)
-# Load your own data here
-INPUT_CONTEXT = "Example context"
-POTENTIAL_RESPONSES = ["Response 1", "Response 2"]
+if FLAGS.model_dir:
+    MODEL_DIR = FLAGS.model_dir
+else:
+    MODEL_DIR = os.path.abspath(os.path.join("./runs", str(TIMESTAMP)))
 
-def get_features(context, utterance):
-    context_matrix = np.array(list(vp.transform([context])))
-    utterance_matrix = np.array(list(vp.transform([utterance])))
-    context_len = len(context.split(" "))
-    utterance_len = len(utterance.split(" "))
-    features = {
-                "context": tf.convert_to_tensor(context_matrix, dtype=tf.int64),
-                "context_len": tf.constant(context_len, shape=[1,1], dtype=tf.int64),
-                "utterance": tf.convert_to_tensor(utterance_matrix, dtype=tf.int64),
-                "utterance_len": tf.constant(utterance_len, shape=[1,1], dtype=tf.int64),
-                }
-    return features, None
+TRAIN_FILE = os.path.abspath(os.path.join(FLAGS.input_dir, "train.tfrecords"))
+VALIDATION_FILE = os.path.abspath(os.path.join(FLAGS.input_dir, "validation.tfrecords"))
 
-if __name__ == "__main__":
+tf.logging.set_verbosity(FLAGS.loglevel)
+
+def main(unused_argv):
     hparams = udc_hparams.create_hparams()
-    model_fn = udc_model.create_model_fn(hparams, model_impl=dual_encoder_model)
-    estimator = tf.contrib.learn.Estimator(model_fn=model_fn, model_dir=FLAGS.model_dir)
-    # Ugly hack, seems to be a bug in Tensorflow
-    # estimator.predict doesn't work without this line
-    estimator._targets_info = tf.contrib.learn.estimators.tensor_signature.TensorSignature(tf.constant(0, shape=[1,1]))
-    print("Context: {}".format(INPUT_CONTEXT))
-    for r in POTENTIAL_RESPONSES:
-        prob = estimator.predict(input_fn=lambda: get_features(INPUT_CONTEXT, r))
-        print("{}: {:g}".format(r, prob[0,0]))
+    model_fn = udc_model.create_model_fn(
+                                         hparams,
+                                         model_impl=dual_encoder_model)
+    estimator = tf.contrib.learn.Estimator(
+                                           model_fn=model_fn,
+                                           model_dir=MODEL_DIR,
+                                           config=tf.contrib.learn.RunConfig())
+    input_fn_train = udc_inputs.create_input_fn(
+                                                mode=tf.contrib.learn.ModeKeys.TRAIN,
+                                                input_files=[TRAIN_FILE],
+                                                batch_size=hparams.batch_size,
+                                                num_epochs=FLAGS.num_epochs)
+    input_fn_eval = udc_inputs.create_input_fn(
+                                               mode=tf.contrib.learn.ModeKeys.EVAL,
+                                               input_files=[VALIDATION_FILE],
+                                               batch_size=hparams.eval_batch_size,
+                                               num_epochs=1)
+    eval_metrics = udc_metrics.create_evaluation_metrics()
+    eval_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+                                                               input_fn=input_fn_eval,
+                                                               every_n_steps=FLAGS.eval_every,
+                                                               metrics=eval_metrics)
+    estimator.fit(input_fn=input_fn_train, steps=None, monitors=[eval_monitor])
+if __name__ == "__main__":
+    tf.app.run()
