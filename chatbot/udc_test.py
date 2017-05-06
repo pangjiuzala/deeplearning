8971a504f1fc72cca3d0f32cfe780dbb5896106d
pangjiuzala <1325530876@qq.com>
2017-05-06 17:40:47 +0800

    first commit

--- chatbot/udc_test.py
+++ chatbot/udc_test.py
@@ -1,3 +1,6 @@
+import os
+import time
+import itertools
 import sys
 import tensorflow as tf
 import udc_model
@@ -13,21 +16,24 @@ tf.flags.DEFINE_integer("test_batch_size", 16, "Batch size for testing")
 FLAGS = tf.flags.FLAGS
 
 if not FLAGS.model_dir:
-    print("You must specify a model directory")
-    sys.exit(1)
-    tf.logging.set_verbosity(FLAGS.loglevel)
+  print("You must specify a model directory")
+  sys.exit(1)
+
+tf.logging.set_verbosity(FLAGS.loglevel)
 
 if __name__ == "__main__":
-    hparams = udc_hparams.create_hparams()
-    model_fn = udc_model.create_model_fn(hparams, model_impl=dual_encoder_model)
-    estimator = tf.contrib.learn.Estimator(
-                                           model_fn=model_fn,
-                                           model_dir=FLAGS.model_dir,
-                                           config=tf.contrib.learn.RunConfig())
-    input_fn_test = udc_inputs.create_input_fn(
-                                               mode=tf.contrib.learn.ModeKeys.EVAL,
-                                               input_files=[FLAGS.test_file],
-                                               batch_size=FLAGS.test_batch_size,
-                                               num_epochs=1)
-    eval_metrics = udc_metrics.create_evaluation_metrics()
-    estimator.evaluate(input_fn=input_fn_test, steps=None, metrics=eval_metrics)
+  hparams = udc_hparams.create_hparams()
+  model_fn = udc_model.create_model_fn(hparams, model_impl=dual_encoder_model)
+  estimator = tf.contrib.learn.Estimator(
+    model_fn=model_fn,
+    model_dir=FLAGS.model_dir,
+    config=tf.contrib.learn.RunConfig())
+
+  input_fn_test = udc_inputs.create_input_fn(
+    mode=tf.contrib.learn.ModeKeys.EVAL,
+    input_files=[FLAGS.test_file],
+    batch_size=FLAGS.test_batch_size,
+    num_epochs=1)
+
+  eval_metrics = udc_metrics.create_evaluation_metrics()
+  estimator.evaluate(input_fn=input_fn_test, steps=None, metrics=eval_metrics)
